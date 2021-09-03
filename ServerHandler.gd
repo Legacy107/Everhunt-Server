b090670a666e626a490864cb347e0d834c77d537
LeStolz <vonamdang03@gmail.com>
2021-09-03 10:44:27 +0700

    Refactor Server
    - Remove EntityHandler because it is useless now
    - Change project name to "EverhuntServer"
    - Refactor ServerHandler
    - Delete Players node in World.tscn cause we dun need it yet

--- ServerHandler.gd
+++ ServerHandler.gd
@@ -3,6 +3,8 @@ extends Node
 
 var network = NetworkedMultiplayerENet.new()
 var port = 1909
+
+
 var max_players = 100
 var player_ids = []
 
@@ -10,33 +12,33 @@ var player_ids = []
 func _ready():
 	network.create_server(port, max_players)
 	get_tree().set_network_peer(network)
-	
+
 	print("Server started")
-	
+
 	network.connect("peer_connected", self, "_peer_connected")
 	network.connect("peer_disconnected", self, "_peer_disconnected")
 
 
 func _peer_connected(player_id):
 	print(str(player_id) + " connected")
-	
+
 	player_ids.append(player_id)
-	
+
 	rpc_id(0, "return_connected_player", player_id)
 	rpc_id(player_id, "return_connected_players", player_ids)
 
 
 func _peer_disconnected(player_id):
 	print(str(player_id) + " disconnected")
-	
+
 	player_ids.erase(player_id)
-	
+
 	rpc_id(0, "return_disconnected_player", player_id)
 
 
-remote func synchronize(func_name, state):
-	rpc_id(0, func_name, get_tree().get_rpc_sender_id(), state)
+remote func synchronize(path, func_name, state):
+	rpc_id(0, "s_synchronize", path, func_name, state)
 
 
-remote func synchronize_unreliable(func_name, state):
-	rpc_id(0, func_name, get_tree().get_rpc_sender_id(), state)
+remote func synchronize_client(path, func_name, state):
+	rpc_id(0, "s_synchronize_client", path, func_name, state, get_tree().get_rpc_sender_id())
